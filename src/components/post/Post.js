953c97877e0c375740dfe83cc5f370099b3eeaa8
Eduardo Rojas <edu.rojas.paez@gmail.com>
2023-10-06 16:29:30 -0500

    Added some parts of the post component

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { MoreVert } from '@mui/icons-material';
+import './post.css'
 
 export default function Post() {
   return (
-    <div>Post</div>
+    <div className='post'>
+      <div className='postWrapper'>
+        <div className='postTop'>
+          <div className='postTopLeft'>
+            <img className='postProfileImage' src='/assets/person/person1.png' alt=''/>
+            <span className='postUsername'>Someone</span>
+            <span className='postDate'>5 min ago</span>
+          </div>
+          <div className='postTopRight'>
+            <MoreVert />
+          </div>
+        </div>
+        <div className='postCenter'>
+
+        </div>
+        <div className='postBottom'>
+
+        </div>
+      </div>
+    </div>
   )
 }
