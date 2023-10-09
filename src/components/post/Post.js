255a6ff4017209d65f76244ed9c6f9f8853b7738
Eduardo Rojas <edu.rojas.paez@gmail.com>
2023-10-09 10:46:00 -0500

    Finished the feed and post components

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -17,10 +17,18 @@ export default function Post() {
           </div>
         </div>
         <div className='postCenter'>
-
+          <span className='postText'>Hey! It's my first post</span>
+          <img className="postImage" src="/assets/post/post1.png" alt=''/>
         </div>
         <div className='postBottom'>
-
+          <div className='postBottomLeft'>
+            <img className='likeIcon' src='/assets/like.png' alt=''/>
+            <img className='likeIcon' src='/assets/heart.png' alt=''/>
+            <span>32 people like it</span>
+          </div>
+          <div className='postBottomRight'>
+            <span className='postCommentText'>9 comments</span>
+          </div>
         </div>
       </div>
     </div>
