188333a395179eac35bcf74278c79ead6658937e
Eduardo Rojas <edu.rojas.paez@gmail.com>
2023-10-11 11:13:34 -0500

    Finished Right Bar component

--- src/components/rightbar/RightBar.js
+++ src/components/rightbar/RightBar.js
@@ -2,6 +2,23 @@ import "./rightbar.css"
 
 export default function RightBar() {
   return (
-    <div className="rightBar">RightBar</div>
+    <div className="rightBar">
+      <div className="rightBarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImage" src="/assets/gift.png" alt=""/>
+          <span className="birthdayText"><b>Someone</b> and <b>3 other friends</b> have a birthday today</span>
+        </div>
+        <h4 className="rightBarTitle">Online Friends</h4>
+        <ul className="rightBarFriendList">
+          <li className="rightBarFriend">
+            <div className="rightBarProfileImageContainer">
+              <img className="rightBarProfileImage" src="/assets/person/person3.png" alt=""/>
+              <span className="rightBarOnline"></span>
+            </div>
+            <span className="rightBarUsername">Someone</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
