cdf901afa555778cb7482904577bd7bb2db0bdd4
Eduardo Rojas <edu.rojas.paez@gmail.com>
2023-11-07 22:14:14 -0600

    feat: added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/login" element={<Login />}>
+        </Route>
+        <Route path="/profile" element={<Profile />}>
+        </Route>
+        <Route path="/register" element={<Register />}>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
