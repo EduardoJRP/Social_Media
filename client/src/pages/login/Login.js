ecdecb09eaf36c91638fd2e4d6c58f242a6cfd21
Eduardo Rojas <edu.rojas.paez@gmail.com>
2023-11-14 20:31:05 -0600

    fix: Changing to tailwindcss

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -13,11 +13,11 @@ export default function Login() {
             </div>
             <div className='loginRight'>
                 <div className='loginBox'>
-                    <input placeholder='Email' className='loginInput'/>
-                    <input placeholder='Password' className='loginInput'/>
-                    <button className='loginButton'>Log In</button>
-                    <span className='loginForgot'>Forgot Password?</span>
-                    <button className='loginRegisterButton'>Create a New Account</button>
+                    <input placeholder='Email' className='h-12 rounded-xl border-2 border-solid border-gray-400 text-lg pl-5 focus:outline-none'/>
+                    <input placeholder='Password' className='h-12 rounded-xl border-2 border-solid border-gray-400 text-lg pl-5 focus:outline-none'/>
+                    <button className='h-12 rounded-xl border-none bg-blue-700 text-white text-xl font-medium cursor-pointer'>Log In</button>
+                    <span className='text-center text-blue-600'>Forgot Password?</span>
+                    <button className='w-3/5 self-center h-12 rounded-xl border-none bg-green-500 text-white text-xl font-medium cursor-pointer'>Create a New Account</button>
                 </div>
             </div>
         </div>
